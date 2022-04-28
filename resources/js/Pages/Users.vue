16be22a8b840ab9fad0f1bbf358ce9fb45478ab5
rama <rama@javan.co.id>
2022-04-29 00:33:39 +0700

    scroll

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,13 +1,20 @@
 <template>
-    <h1>Users</h1>
+    <h1 class="text-4xl font-bold">Users</h1>
 
     <Nav />
+
+    <div style="margin-top: 400px">
+        <p>The current time is {{ time }}.</p>
+        <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
+    </div>
 </template>
 
 <script>
 import Nav from "../Shared/Nav";
+import { Link } from "@inertiajs/inertia-vue3";
 
 export default {
-    components: { Nav },
+    components: { Nav, Link },
+    props: { time: String },
 };
 </script>
