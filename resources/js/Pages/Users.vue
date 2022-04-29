444b24f093e5b287111c1616ff4fa4039ac3bd97
rama <rama@javan.co.id>
2022-04-29 13:40:17 +0700

    Pagination

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,14 +1,81 @@
 <template>
-    <Head title="Users"/>
+    <Head title="Users" />
 
     <h1 class="text-3xl">Users</h1>
 
-    <div style="margin-top: 400px">
-        <p>The current time is {{ time }}.</p>
-        <Link href="/users" class="text-blue-500" preserve-scroll>Refresh</Link>
+    <div class="flex flex-col mt-6">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div
+                class="
+                    py-2
+                    align-middle
+                    inline-block
+                    min-w-full
+                    sm:px-6
+                    lg:px-8
+                "
+            >
+                <div
+                    class="
+                        shadow
+                        overflow-hidden
+                        border-b border-gray-200
+                        sm:rounded-lg
+                    "
+                >
+                    <table
+                        class="min-w-full divide-y divide-gray-200"
+                        aria-hidden="true"
+                    >
+                        <tbody class="bg-white divide-y divide-gray-200">
+                            <tr v-for="user in users.data" :key="user.id">
+                                <td class="px-6 py-4 whitespace-nowrap">
+                                    <div class="flex items-center">
+                                        <div
+                                            class="
+                                                text-sm
+                                                font-medium
+                                                text-gray-900
+                                            "
+                                        >
+                                            {{ user.name }}
+                                        </div>
+                                    </div>
+                                </td>
+
+                                <td
+                                    class="
+                                        px-6
+                                        py-4
+                                        whitespace-nowrap
+                                        text-right text-sm
+                                        font-medium
+                                    "
+                                >
+                                    <Link
+                                        class="
+                                            text-indigo-600
+                                            hover:text-indigo-900
+                                        "
+                                        :href="`/users/${user.id}/edit`"
+                                    >
+                                        Edit
+                                    </Link>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
     </div>
+
+    <!-- paginator -->
+    <Pagination :links="users.links" class="mt-6" />
 </template>
 
 <script setup>
-defineProps({time: String})
+import Pagination from "../Shared/Pagination.vue";
+
+defineProps({ users: Object });
 </script>
