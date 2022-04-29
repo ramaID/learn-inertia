71114f7f03b59043dde115c3cc1de833919b6a6d
rama <rama@javan.co.id>
2022-04-29 11:26:12 +0700

    layout

---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="p-6 bg-gray-300">
+        <header class="flex justify-between">
+            <h1 class="font-bold text-lg">My App</h1>
+
+            <Nav />
+        </header>
+    </section>
+
+    <section class="p-6">
+        <div class="max-w-2xl mx-auto ">
+            <slot />
+        </div>
+    </section>
+</template>
+
+<script>
+import Nav from "./Nav";
+
+export default {
+    components: { Nav },
+};
+</script>
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,9 +11,7 @@
 </head>
 
 <body>
-    <section class="px-8">
-        @inertia
-    </section>
+    @inertia
 </body>
 
 </html>
